13d3029bf9a90fdbb884ca6318ed14d3e6ec940d
May W <51608792+May-W@users.noreply.github.com>
2019-08-23 06:13:04 -0500

    Half way working search controller

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,15 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "/results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        if (searchType.equals("all")) {
+
+        } else {
+
+        }
+
+    }
 
 }
